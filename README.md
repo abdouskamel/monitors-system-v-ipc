34bbf23ca3470fe51323e7c2dba2b8456df0ef60
abdouskamel <abdous.kamel67@gmail.com>
2023-11-17 13:01:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Implementation of Hoare monitors using System V IPC (InterProcess Communication)
+
+```
+gcc -o prog main.c monitor.c
+./prog
+```
---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include <string.h>
+#include <errno.h>
+
+#include "monitor.h"
+
+#define NB_CONDS 1
+
+typedef struct
+{
+    int arrived;
+
+} shm_mem;
+
+int main()
+{
+    monitor mtor;
+    create_monitor("p", 1, 2, NB_CONDS, sizeof(shm_mem), &mtor);
+
+    // Create first process
+    if (!fork())
+    {
+        printf("P1 starts.\n");
+
+        // init_monitor not necessary, just for the example
+        init_monitor("p", 1, 2, &mtor);
+
+        sleep(1);
+        enter_monitor(&mtor);
+
+        shm_mem *shm_ptr = mtor_shmat(&mtor);
+
+        // Don't wait if P2 arruved
+        if (!shm_ptr->arrived)
+        {
+            printf("P1 waits for P2.\n");
+            mtor_wait(&mtor, 0);
+        }
+
+        printf("P1 continues, arrived = %d\n", shm_ptr->arrived);
+        mtor_shmdt(shm_ptr);
+
+        exit_monitor(&mtor);
+        exit(0);
+    }
+
+    // Create second process
+    if (!fork())
+    {
+        printf("P2 starts.\n");
+
+        // init_monitor not necessary, just for the example
+        init_monitor("p", 1, 2, &mtor);
+
+        sleep(3);
+        enter_monitor(&mtor);
+
+        shm_mem *shm_ptr = mtor_shmat(&mtor);
+        shm_ptr->arrived = 1;
+        mtor_shmdt(shm_ptr);
+
+        printf("P2 wakes P1.\n");
+        mtor_signal(&mtor, 0);
+
+        exit_monitor(&mtor);
+        exit(0);
+    }
+
+    while (wait(NULL) != -1)
+        ;
+
+    free_monitor(&mtor);
+    return (0);
+}
